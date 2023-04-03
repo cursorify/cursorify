a52eb5497cbd1e23801a6cdb64c9f54fbc810cbc
morethanmin <mini4614@gmail.com>
2023-04-03 23:52:30 +0900

    feat: update website

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { Global } from '@emotion/react'
 import { routes } from '@routes/index'
 import { globalStyles } from '@styles/global'
-import { BASENAME } from './constants'
-
-const basename = process.env.NODE_ENV === 'development' ? '' : BASENAME
 
 const App: React.FC = () => {
-  const router = createBrowserRouter(routes, {
-    basename,
-  })
+  const router = createHashRouter(routes)
 
   return (
     <>
