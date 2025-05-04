4cbcfcf7d7d392bd241e58249948821a1705f6c9
min-prnd <min@prnd.co.kr>
2025-05-04 18:21:36 +0900

    fix: update TypeScript type references and optimize mouse movement effect

--- src/components/CursorifyProvider/Cursorify/useMouseMoveEffect.ts
+++ src/components/CursorifyProvider/Cursorify/useMouseMoveEffect.ts
@@ -28,8 +28,8 @@ const useMouseMoveEffect = (mouseRef: RefObject<HTMLDivElement | null>) => {
   const endX = useRef(0)
   const endY = useRef(0)
 
-  const _x: React.MutableRefObject<number | null> = useRef(null)
-  const _y: React.MutableRefObject<number | null> = useRef(null)
+  const _x: React.RefObject<number | null> = useRef(null)
+  const _y: React.RefObject<number | null> = useRef(null)
 
   const requestRef = useRef(0)
 
@@ -38,9 +38,9 @@ const useMouseMoveEffect = (mouseRef: RefObject<HTMLDivElement | null>) => {
     if (_x.current !== null && _y.current !== null) {
       _x.current += (endX.current - _x.current) / delay
       _y.current += (endY.current - _y.current) / delay
+      mouseRef.current.style.opacity = `${opacity}`
+      mouseRef.current.style.transform = `translate(-50%, -50%) translate3d(${_x.current}px, ${_y.current}px, 0)`
     }
-    mouseRef.current.style.opacity = `${opacity}`
-    mouseRef.current.style.transform = `translate(-50%, -50%) translate3d(${_x.current}px, ${_y.current}px, 0)`
 
     requestRef.current = requestAnimationFrame(animateMouse)
   }
